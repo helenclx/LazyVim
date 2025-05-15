3b86cff198259914eb81bf10533a5af987cad8af
Helen Chong <contact@helenchong.dev>
2025-05-16 00:31:08 +0800

    Add my custom configurations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+LazyVim.terminal.setup("pwsh")
+
+vim.opt.shiftwidth = 4
+vim.opt.wrap = true
+vim.opt.relativenumber = false
+
+vim.o.tabstop = 4
+vim.o.conceallevel = 0
