a18c9d6b92ebc178248715b636b73402404a6459
Helen Chong <contact@helenchong.dev>
2025-05-20 08:51:47 +0800

    Add comment about commenting out setting Powershell as default shell

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Comment out this line if you are not using Windows
 LazyVim.terminal.setup("pwsh")
 
 vim.opt.shiftwidth = 4
